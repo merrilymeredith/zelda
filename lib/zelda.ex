16dc03f45b4d78fe83b88a85a8686b86f6659eb4
Meredith Howard <mhoward@roomag.org>
2015-08-27 01:35:09 -0400

    Move main supervisor into top-level

--- lib/zelda.ex
+++ lib/zelda.ex
@@ -1,7 +1,17 @@
 defmodule Zelda do
   use Application
 
+  @api_token Application.get_env(:zelda, :slack_token)
+
   def start(_type, _args) do
-    Zelda.Supervisor.start_link
+    import Supervisor.Spec
+
+    children = [
+      worker(Zelda.Repo, []),
+      worker(Zelda.State, [:last_id]),
+      worker(Zelda.Slack, [@api_token]),
+    ]
+
+    children |> supervise(name: Zelda.Sup, strategy: :one_for_one)
   end
 end
