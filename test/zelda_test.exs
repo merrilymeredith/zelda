c75fa9ad9ae745f2f7b43eaaeb03a9e6758b44d3
Meredith Howard <mhoward@roomag.org>
2015-09-05 11:33:57 -0400

    Make sure Repo is started by the time we want to test it.

--- test/zelda_test.exs
+++ test/zelda_test.exs
@@ -45,6 +45,9 @@ defmodule ZeldaTest do
     slack_id   = "U123456789"
     slack_name = "quux"
 
+    # Not started sometimes by the time we get to this test?
+    Zelda.Repo.start_link
+
     Zelda.Repo.transaction fn ->
       refute Ignore.is_ignored?(:slack_id, slack_id)
 
