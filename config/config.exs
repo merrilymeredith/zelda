14cdf71e08acdba2c4582f3e5e8c2138f8cc4ea9
Meredith Howard <mhoward@roomag.org>
2015-09-05 13:03:30 -0400

    Make the :match key a keyword list

--- config/config.exs
+++ config/config.exs
@@ -1,11 +1,9 @@
 use Mix.Config
 
-config :zelda,
-  match: %{
-    link:    ~r/\b(([a-z_]+):([\w-]+))\b/,
-    repeat:  ~r/\b([a-z_]+:!\$)/,
-    command: ~r/^zelda:\s*(\w+)\s*(.*)$/,
-  }
+config :zelda, :match,
+  link:    ~r/\b(([a-z_]+):([\w-]+))\b/,
+  repeat:  ~r/\b([a-z_]+:!\$)/,
+  command: ~r/^zelda:\s*(\w+)\s*(.*)$/
 
 config :zelda,
   slack_token: System.get_env("SLACK_API_TOKEN")
